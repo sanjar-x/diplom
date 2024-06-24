91e3b17349f93c977a329e7c7b29cba3f55fe0c0
sanjar-x <sanjar68x@gmail.com>
2024-06-24 22:53:41 +0500

    Add URL for update tables

---
+++ app/api/endpoints/settings.py
@@ -0,0 +1,15 @@
+from fastapi import APIRouter
+from app.api.dependencies.database_session import engine
+from app.models.models import Base
+
+router = APIRouter(prefix="/auth")
+
+
+@router.get("/reset")
+async def get_token_for_user():
+    async with engine.begin() as connection:
+
+        await connection.exec_driver_sql("DROP SCHEMA public CASCADE")
+        await connection.exec_driver_sql("CREATE SCHEMA public")
+        await connection.run_sync(Base.metadata.create_all)
+    return {"message": "Reseted"}
--- app/api/routers.py
+++ app/api/routers.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from .endpoints import role, user, faculty, department, division, employees
+from .endpoints import role, user, faculty, department, division, employees, settings
 
 api_router = APIRouter()
 api_router.include_router(role.router, tags=["roles"])
@@ -8,3 +8,4 @@ api_router.include_router(faculty.router, tags=["faculties"])
 api_router.include_router(department.router, tags=["departments"])
 api_router.include_router(division.router, tags=["divisions"])
 api_router.include_router(employees.router, tags=["employees"])
+api_router.include_router(settings.router, tags=["settings"])
